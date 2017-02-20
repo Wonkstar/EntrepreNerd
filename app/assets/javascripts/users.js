591fd4a2f55328792742b791515eebf13eab8feb
William Scott <will_scott004@hotmail.com>
2017-02-20 23:27:08 +0000

    added Stripe reponse handler and error handling

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,18 +12,56 @@ $(document).on('turbolinks:load', function(){
   submitBtn.click(function(event){
     // Prevent default button behaviour
     event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
     
     var ccNum = $('#card_number').val(), 
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
     
+    // Validate card details
+    var error = false;
+    
+    //Validate card number.
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+    //Validate CVC number.
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+    //Validate expiration date.
+    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+    
     // Send the card info to Stripe
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponceHandler);
+    if (error) {
+      submitBtn.prop('disabled', false).val("Sign Up");
+    }
+    else {
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponceHandler);
+    }
+    return false;
   });
+  
+  //Stripe will return a card token.
+  function stripeResponceHandler(status, resopnse) {
+    // Get token from responce
+    var token = response.id;
+    
+    // Inject the card token in a hidden field
+    proForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token));
+    
+    // Submit form to our rails app
+    proForm.get(0).submit();
+  }
 })
