44b59f65c7339dd8d0bc8d62f8fc161096e7bf6d
William Scott <will_scott004@hotmail.com>
2017-02-21 04:59:09 +0000

    attempting to fix migration issue

---
+++ db/migrate/20170221045712_remove_plan_from_user.rb
@@ -0,0 +1,5 @@
+class RemovePlanFromUser < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :plan_id, :integer
+  end
+end
---
+++ db/migrate/20170221045807_add_plan_to_user_again.rb
@@ -0,0 +1,5 @@
+class AddPlanToUserAgain < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :plan_id, :integer
+  end
+end
